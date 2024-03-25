5c070231f623c19eaa076e0ca5d3d12729326808
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2024-03-25 10:33:31 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li><a href="profile">Profile</a></li>
+          <li><a href="dashboard">Dashboard</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
